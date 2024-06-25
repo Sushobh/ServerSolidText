8ad89522a980e9157ac6c3b02ad4a3df09ab7ff8
Sushobh <aryansushobh@gmail.com>
2024-06-25 22:48:56 +0530

    MultiModule spring app

---
+++ common/src/main/kotlin/common/util/STValidation.kt
@@ -0,0 +1,14 @@
+package com.sushobh.common.util
+
+import org.springframework.stereotype.Component
+
+
+@Component
+class STValidation {
+
+    
+    fun isEmailValid(emailText : String){
+
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Readme 
+
+#### Running through command line
+`./gradlew bootRun` Before this make sure that the java version on the command line
+is pointing to the right java version. 
