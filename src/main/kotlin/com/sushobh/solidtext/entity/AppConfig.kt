1a6b64f5f031cb23ca02e0d03fec5895cbf18e29
Sushobh <aryansushobh@gmail.com>
2024-07-07 13:05:40 +0530

    S? Changes

--- src/main/kotlin/com/sushobh/solidtext/entity/AppConfig.kt
+++ src/main/kotlin/com/sushobh/solidtext/entity/AppConfig.kt
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 const val BASE_URL = "http://10.0.2.2:8080"
 
 data class AppConfig (
-    @JsonProperty("LOGIN_URL") val loginUrl : String = BASE_URL+"/login"
+    @JsonProperty("SIGNUP_URL") val loginUrl : String = "$BASE_URL/public/signup"
 )
 
 
