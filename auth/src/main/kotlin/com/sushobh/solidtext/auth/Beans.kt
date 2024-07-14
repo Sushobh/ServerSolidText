52008405648add62330b6cc8cf1aa8e50da3d0dc
Sushobh <aryansushobh@gmail.com>
2024-07-14 19:09:06 +0530

    S? changed password encoder strength

--- auth/src/main/kotlin/com/sushobh/solidtext/auth/Beans.kt
+++ auth/src/main/kotlin/com/sushobh/solidtext/auth/Beans.kt
@@ -12,7 +12,7 @@ open class Configuration {
 
 
     @Bean
-    open fun passwordEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder(16)
+    open fun passwordEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder(4)
 
     @Qualifier("loginTokenConfig")
     @Bean
