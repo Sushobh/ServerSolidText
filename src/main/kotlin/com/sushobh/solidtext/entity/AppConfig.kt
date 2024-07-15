69e07e7ac6c3f02c61a91f019d50784b3b4ea3af
Sushobh <aryansushobh@gmail.com>
2024-07-15 14:14:20 +0530

    S? login url

--- src/main/kotlin/com/sushobh/solidtext/entity/AppConfig.kt
+++ src/main/kotlin/com/sushobh/solidtext/entity/AppConfig.kt
@@ -7,8 +7,9 @@ import com.fasterxml.jackson.annotation.JsonProperty
 const val BASE_URL = "http://10.0.2.2:8080"
 
 data class AppConfig (
-    @JsonProperty("SIGNUP_URL") val loginUrl : String = "$BASE_URL/public/signup",
-    @JsonProperty("OTP_VALIDATE_URL") val otpValidateUrl : String = "$BASE_URL/public/otpValidate"
+    @JsonProperty("SIGNUP_URL") val signupUrl : String = "$BASE_URL/public/signup",
+    @JsonProperty("OTP_VALIDATE_URL") val otpValidateUrl : String = "$BASE_URL/public/otpValidate",
+    @JsonProperty("LOGIN_URL") val loginUrl : String = "$BASE_URL/public/login"
 )
 
 
